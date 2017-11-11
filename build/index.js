dd11d80c0cb9e7f92ff2c2d4dc54703d3018f0c2
Sneezoo <blue.xdimension@gmail.com>
2017-11-11 20:10:07 +0100

    Update version and rebuild index.js

--- build/index.js
+++ build/index.js
@@ -20,7 +20,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
+ *
  */
 const BABELRC_FILENAME = '.babelrc';
 const BABELRC_JS_FILENAME = '.babelrc.js';
